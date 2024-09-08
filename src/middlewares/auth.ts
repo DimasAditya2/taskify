0b4770120b186c9c0c125266d716c4b1dc710d28
DimasAditya2 <esadimasadityaa@gmail.com>
2024-09-08 09:24:12 +0700

    fix: auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,7 +4,7 @@ import { taskModel } from '../models/task.model'
 export const authentication = (req: Request, res: Response, next: NextFunction) => {
   const user = res.locals.user
 
-  if (!user) return res.sendStatus(403)
+  if (!user) return res.sendStatus(403).send({status: 401, statusCode: 401, message: `Forbidden: You are not allowed to access, please log in first`})
 
   return next()
 }
