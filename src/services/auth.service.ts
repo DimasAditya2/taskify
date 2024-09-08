56aa777acbd1756e12a01ca696b4195a029fdba5
DimasAditya2 <esadimasadityaa@gmail.com>
2024-09-08 08:58:44 +0700

    feat: deploy vercel

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -9,10 +9,9 @@ export const registerUser = async (payload: UserType) => {
     if (error.code === 11000) {
       logger.info('Error: 409 Conflict - Duplicate Key:', error)
       throw new Error('Email already exists')
-    } else {
-      logger.info('Error service register', error)
-      throw error
     }
+    logger.info('Error service register', error)
+    throw error
   }
 }
 
@@ -20,7 +19,6 @@ export const findUserByEmail = async (email: string) => {
   try {
     return await userModel.findOne({ email })
   } catch (error) {
-    console.log(error)
     logger.info('Error service login', error)
     throw error
   }
